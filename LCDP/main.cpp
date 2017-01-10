be20bb073087b00e272c580010608d2fe48f07a1
Matthew <Matthew@MATTHEWCHIN>
2017-01-11 01:55:39 +0800

    Completed OpenCV Testing

--- LCDP/main.cpp
+++ LCDP/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "BackgroundSubtractorLCDP.h"
+#include <opencv2\opencv.hpp>
 
 int main() {
 	
 	int a = 1;
+	cv::Mat img = cv::imread("test.png");
+	cv::imshow("Test", img);
+	cv::waitKey();
 	return 0;
 }
